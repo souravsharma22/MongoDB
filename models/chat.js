f22373505854c5986ebae6276f741851cee896ae
souravsharma2210 <souravbgp2210@gmail.com>
2025-10-01 17:42:30 +0530

    Modules creations

---
+++ models/chat.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema({
+    from:{
+        type: String,
+        required: true
+    },
+    to:{
+        type:String,
+        required: true,
+    },
+    msg:{
+        type: String,
+        maxLength: 50,
+    },
+    created_at:{
+        type:Date,
+        required: true
+    }
+})
+
+const chat = mongoose.model("chat",chatSchema  );
+export default chat;
